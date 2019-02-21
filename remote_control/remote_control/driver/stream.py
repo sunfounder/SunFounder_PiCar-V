dda37fd54cf782a58b8d81573cbcb594bd9de0cc
Izhar Firdaus <kagesenshi.87@gmail.com>
2019-02-21 17:26:53 +0800

    undo fedberry specific changes

--- remote_control/remote_control/driver/stream.py
+++ remote_control/remote_control/driver/stream.py
@@ -15,17 +15,13 @@ import tempfile
 import subprocess
 import os
 
-_CODE_DIR_ = os.path.join(os.path.dirname(__file__), '..', '..', ',,')
+_CODE_DIR_ = "/home/pi/SunFounder_PiCar-V"
 
 MJPG_STREAMER_PATH = "mjpg_streamer"
-INPUT_PATH = "/usr/local/lib/mjpg-streamer/input_uvc.so"
-OUTPUT_PATH = "/usr/local/lib/mjpg-streamer/output_http.so -w /var/www/mjpeg_streamer"
+INPUT_PATH = "/usr/local/lib/input_uvc.so"
+OUTPUT_PATH = "/usr/local/lib/output_http.so -w /usr/local/www"
 
-dev_files = os.listdir('/dev')
-video_files = sorted([f for f in dev_files if 'video' in f])
-if not video_files:
-   raise IOError("Camera is not connected correctly")
-stream_cmd = '%s -i "%s -d /dev/%s" -o "%s" &' % (MJPG_STREAMER_PATH, INPUT_PATH, video_files[0], OUTPUT_PATH)
+stream_cmd = '%s -i "%s" -o "%s" &' % (MJPG_STREAMER_PATH, INPUT_PATH, OUTPUT_PATH)
 
 def run_command(cmd):
 	with tempfile.TemporaryFile() as f:
@@ -36,11 +32,10 @@ def run_command(cmd):
 
 def start():
 	files = os.listdir('/dev')
-	print(stream_cmd)
-	video_files = [f for f in files if 'video' in f]
-	if not video_files:
+	if 'video0' in files:
+		run_command(stream_cmd)
+	else:
 		raise IOError("Camera is not connected correctly")
-	run_command(stream_cmd)
 
 def get_host():
 	return run_command('hostname -I')
