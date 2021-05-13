117b466f59691d6b84d17fe556b02f5761f99389
xiemeiping <1035791926@qq.com>
2021-05-13 16:17:23 +0800

    add docments

---
+++ docs/source/_build/html/ja_JA/_sources/getting_on_the_road.rst.txt
@@ -0,0 +1,15 @@
+Getting on the Road!
+====================
+
+Make sure all the procedures have been finished, and check there is no
+problem about the mechanical assembly and software installation.
+
+
+
+.. toctree::
+   :maxdepth: 2
+      
+   remote_control
+   ball_tracker
+   file_analysis
+   advanced
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,14 +13,17 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+
+
 import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'PiCar-V'
-copyright = '2021, sunfounder'
-author = 'sunfounder'
+project = 'SunFounder ESP-4WD Car Kit'
+copyright = '2021, SunFounder'
+author = 'SunFounder'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,7 +31,7 @@ author = 'sunfounder'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,11 +47,17 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+latex_engine = 'pdflatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
